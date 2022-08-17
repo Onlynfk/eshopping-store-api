ec185b36ccbfa00db22cc2209ba048a1b0e765c0
OnlynfK <onlygod15@hotmail.com>
2022-08-17 09:37:05 +0100

    updated

--- core/models.py
+++ core/models.py
@@ -29,7 +29,7 @@ class Categories(models.TextChoices):
 
 class Product(models.Model):
     """A model that contains data for a single product."""
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True,  null=True )
     price = models.DecimalField(max_digits=8, decimal_places=2)
     name = models.CharField(max_length=255, help_text="Product Name")
     store = models.ForeignKey('Store', null=True, related_name="add_products", blank=True, on_delete=models.CASCADE)
@@ -104,7 +104,7 @@ class Order(models.Model):
 
 
 class Store(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
     rating = models.PositiveIntegerField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     all_orders = models.ManyToManyField(OrderItem, related_name= "placed_orders")
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from .models import Store, Product
 # Create your tests here.
+class StoreModelTestcase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Store.objects.create(name="Tecno")
+
+    def test_string_method(self):
+        store = Store.objects.get(name="Tecno")
+        expected_string = f"{store.name}"
+        self.assertEqual(str(store), expected_string)
+
+class ProductModelTestcase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(name="Visa", price="1.34")
+
+    def test_string_method(self):
+        product = Product.objects.get(name="Visa")
+        expected_string = f"{product.name}"
+        self.assertEqual(str(product), expected_string)
