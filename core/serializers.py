210eae69b9759e239da8d586c7c4340498105896
OnlynfK <onlygod15@hotmail.com>
2022-08-17 08:59:13 +0100

    updated

--- core/serializers.py
+++ core/serializers.py
@@ -10,23 +10,26 @@ class OrderItemSerializer(serializers.ModelSerializer):
         fields = ['id', 'store', 'ordered', 'state','quantity']
 
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','name', 'owner', 'price','date_created']
+   
 
 class StoreSerializer(serializers.ModelSerializer):
+    products = OrderItemSerializer(read_only=True, many=True)
+
     class Meta:
         model = Store
-        fields = ['id','name', 'owner', 'date_created', 'all_orders']
+        fields = ['id','name', 'owner', 'date_created', 'all_orders', 'products']
 
+   
 class StoreOrdersSerializer(serializers.ModelSerializer):
     orders = OrderItemSerializer(read_only=True, many=True)
     class Meta:
         model = Store
         fields = ['orders']
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id','name', 'owner', 'price','date_created']
-        
+  
         
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(read_only=True, many=True)
